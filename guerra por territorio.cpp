971cd030d809abfcdd158f38fdbf31a1a1720c50
Luciana Maria Muniz <lucianamuniz90@gmail.com>
2017-07-19 20:00:21 -0300

    Add files via upload

---
+++ guerra por territorio.cpp
@@ -0,0 +1,38 @@
+//guerra por territorio
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+    int main (){
+    int entradas,i,partes, u, soma, area_metade; 
+    soma=0;
+    area_metade=0;
+    cin>>entradas;
+    int secoes[entradas];
+    //povoando o bendito vetor
+    for (i=0; i<entradas; i++){
+        cin>>partes;
+        secoes[i]=partes;
+         
+    }
+     
+    //teste se tá povoadinho
+    for (i=0; i<entradas; i++){
+    }
+     
+    //tentar somar as paradas
+    for (i=0; i<entradas; i++){
+    soma = soma + secoes[i];
+    }
+     
+    //teste da divisão
+         
+            for (i=0; i<entradas; i++){
+            area_metade += secoes[i];
+            if(area_metade==soma/2){
+            cout<<i+1<<endl;
+            }
+            }
+     
+    return 0;
+}
