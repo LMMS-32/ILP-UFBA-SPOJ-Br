ec2d436efe5922cba29a5eaf82f8bb240fa38151
Luciana Maria Muniz <lucianamuniz90@gmail.com>
2017-07-19 20:48:55 -0300

    Add files via upload

---
+++ tacografo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	int N, T, V, D =0;
+//	cin >> N;
+	//while (N<=1000){
+		cin>>N;
+		for (int i=0; i<N; i++)
+		cin >> T >> V;
+		//D = T*V;
+		
+		cout << T*V<<endl;
+	
+	
+	
+	
+	return 0;
+}
